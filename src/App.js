75baff34a18ff7aea981bbf6ce642d406edb40ba
Dhruv Batra <dhruvbatra2@gmail.com>
2022-11-07 18:11:30 -0500

    file structure and tabs

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './App.css';
+import Tabs from './Components/Tabs'
 
 function App() {
   return (
@@ -11,56 +12,6 @@ function App() {
   );
 }
 
-class Tabs extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      activeTab: "graph",
-    };
-  }
-  handleClick = (newTab) => {
-    this.setState({ activeTab: newTab });
-    console.log("clicked", newTab);
-  }
-  render() {
-    const activeTab = this.state.activeTab;
-    console.log(activeTab)
-    return (
-      <div className='tabs'>
-        <ul className='nav'>
-          <li
-            className={activeTab === "graph" ? 'active' : ''}
-            onClick={() => this.handleClick("graph")}>
-            Graph
-            </li>
-          <li
-            className={activeTab === "actions" ? 'active' : ''}
-            onClick={() => this.handleClick("actions")}>
-              Actions
-            </li>
-        </ul>
-        <div className='content'>
-          {this.state.activeTab === "graph" ? <GraphsTab /> : <ActionTab />}
-        </div>
-      </div>
-    );
-  }
-}
 
-const GraphsTab = () => {
-  return (
-      <div className ='GraphsTab'>
-          <p>Graphs Tab Content</p>
-      </div>
-  )
-}
-
-const ActionTab = () => {
-  return (
-      <div className ='ActionTab'>
-          <p>Action Tab Content</p>
-      </div>
-  )
-}
 
 export default App;

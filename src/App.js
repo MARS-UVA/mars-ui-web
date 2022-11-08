6422e7ef89021c9348181f350a145df1c63678c1
Dhruv Batra <dhruvbatra2@gmail.com>
2022-11-08 04:41:09 -0500

    button panel with drive mode and estop

--- src/App.js
+++ src/App.js
@@ -6,12 +6,15 @@ import Grid from '@mui/material/Grid';
 
 import TabContainer from './Components/TabContainer'
 import CameraPane from './Components/Camera.js'
+import ButtonPanel from './Components/ButtonPanel';
 
 function App() {
   return (
     <div className="App">
       <Typography variant="h2">MARS Web UI</Typography>
       <br/>
+      <ButtonPanel/>
+      <br/><br/>
       <Grid container columnSpacing={3}>
         <Grid item>
           <CameraPane cameraType='1'/>

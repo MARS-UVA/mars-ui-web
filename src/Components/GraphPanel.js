9fb0a8fb0c958eb7438e2f47f57ceae8605f2d2b
Dhruv Batra <dhruvbatra2@gmail.com>
2022-11-08 02:00:37 -0500

    show data values toggle checkbox

--- src/Components/GraphPanel.js
+++ src/Components/GraphPanel.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import Checkbox from '@mui/material/Checkbox';
 import FormControlLabel from '@mui/material/FormControlLabel';
+import Grid from '@mui/material/Grid';
 
 export default function GraphPanel(){
 
@@ -10,13 +11,13 @@ export default function GraphPanel(){
     //function that changes variable of whether data collection is toggled based on checkbox input
     const handleDataCollectionToggle = (event) => {
         setDataCollection(!dataCollection);
-        //DO NOT DO GRPC STUFF HERE (unless you want to write async code)
+        //DO NOT CALL GRPC FUNCTION HERE (unless you want to write async code)
     };
 
     //Hook that activates on change of Data Collection State
     //TODO: GRPC Call that enables data collection
     useEffect(()=>{
-        console.log("Data Collection Status: "+dataCollection)
+        console.log("Motor Data Collection Status: "+dataCollection)
         if(dataCollection){
             console.log("enable") //REPLACE ME
         }else{
@@ -24,12 +25,41 @@ export default function GraphPanel(){
         }
     },[dataCollection])
 
+    //Variable for whether motor data collection is toggled
+    const[showData, setShowData] = React.useState(true);
+
+    //function that changes variable of whether data collection is toggled based on checkbox input
+    const handleShowDataToggle = (event) => {
+        setShowData(!showData);
+        //DO NOT CALL GRPC FUNCTION HERE (unless you want to write async code)
+    };
+
+    //Hook that activates on change of Data Collection State
+    //TODO: GRPC Call that enables data collection
+    useEffect(()=>{
+        console.log("Show Motor Value Status: "+showData)
+        if(showData){
+            console.log("enable") //REPLACE ME
+        }else{
+            console.log("disable") //REPLACE ME
+        }
+    },[showData])
+
     return(
     <div>
-        <FormControlLabel control={ <Checkbox
-            checked={dataCollection}
-            onChange={handleDataCollectionToggle}
-            inputProps={{ 'aria-label': 'controlled' }}
-        /> } label="Toggle Data Collection" />
+        <Grid container>
+            <FormControlLabel control={ <Checkbox
+                checked={dataCollection}
+                onChange={handleDataCollectionToggle}
+                inputProps={{ 'aria-label': 'controlled' }}
+            /> } label="Toggle Motor Data Collection" />
+        </Grid>
+        <Grid container>
+            <FormControlLabel control={ <Checkbox
+                checked={showData}
+                onChange={handleShowDataToggle}
+                inputProps={{ 'aria-label': 'controlled' }}
+            /> } label="Toggle Show Motor Values" />
+        </Grid>
     </div>);
 }

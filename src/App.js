6033554ceb29bf8fd1e945b8443ed4b9d4db13c0
ayun1998-3 <77704153+ayun1998-3@users.noreply.github.com>
2023-02-02 16:24:04 -0500

    Updated app.js with react-gamepad implementation
    the display of button and axis values are still very rough

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useGamepads } from 'react-gamepads';
 // import ReactDOM from 'react-dom/client';
 import './App.css';
 import Typography from '@mui/material/Typography';
@@ -8,32 +9,41 @@ import TabContainer from './Components/TabContainer'
 import CameraPane from './Components/CameraPane.js'
 import ButtonPanel from './Components/ButtonPanel';
 
-import {connecthandler, disconnecthandler} from './gamepad.js';
-
 function App() {
+  const [gamepads, setGamepads] = useState({});
+  useGamepads(gamepads => setGamepads(gamepads));
+
+  var message1 = 'nothing is pressed';
+  var message2 = 'Axis';
+  gamepads[0].buttons.forEach((button, i) => {
+    if (button.pressed) {
+      // console.log("Button " + i + " is pressed");
+      message1 = "Button " + i + " is pressed";
+    }
+  });
+
+  gamepads[0].axes.forEach((axis, i) => {
+    message2 = "Axis " + i + ": " + axis;
+  });
+
   return (
     <div className="App">
       <Typography variant="h2">MARS Web UI</Typography>
-      <br/>
-      <ButtonPanel/>
-      <br/><br/>
+      <br />
+      <ButtonPanel />
+      <br /><br />
       <Grid container columnSpacing={3}>
         <Grid item>
-          <CameraPane cameraType='1'/>
-          <br/>
-          <CameraPane cameraType='2'/>
+          <CameraPane cameraType='1' />
+          <br />
+          <CameraPane cameraType='2' />
         </Grid>
-        <Grid item><TabContainer/></Grid>
+        <Grid item><TabContainer /></Grid>
       </Grid>
+      <div>{gamepads[0] ? message1 : ''}</div>
+      <div>{gamepads[0] ? message2 : ''}</div>
     </div>
   );
 }
-
-function connected(e) {
-  console.log('GAMEPAD connected');
-}
-// console.log(connecthandler);
-window.addEventListener("gamepadconnected", connecthandler);
-window.addEventListener("gamepaddisconnected", disconnecthandler);
 
 export default App;

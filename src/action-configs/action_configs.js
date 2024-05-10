2a9a05caf6d7fe7e5992c8c0572d754f504aadf4
MARS at UVA <hoosmininguva@gmail.com>
2024-05-09 22:09:18 -0400

    fixed autonomous action calls

--- src/action-configs/action_configs.js
+++ src/action-configs/action_configs.js
@@ -1,7 +1,7 @@
 var raiseLadderConfig = "{ \
     \"name\": \"raise_ladder\", \
     \"update_delay\": 0.05, \
-    \"speed\": 50, \
+    \"speed\": 10, \
     \"raised_angle\": 52.0 \
 }";
 export { raiseLadderConfig };
@@ -9,7 +9,7 @@ export { raiseLadderConfig };
 var lowerLadderConfig = "{ \
     \"name\": \"lower_ladder\", \
     \"update_delay\": 0.1, \
-    \"speed\": 70, \
+    \"speed\": 10, \
     \"raised_angle\": 45.0 \
 }";
 export { lowerLadderConfig };
@@ -17,14 +17,14 @@ export { lowerLadderConfig };
 var raiseBinConfig = "{ \
     \"name\": \"raise_bin\", \
     \"update_delay\": 0.05, \
-    \"speed\": 30 \
+    \"speed\": 10 \
 }";
 export { raiseBinConfig };
 
 var lowerBinConfig = "{ \
     \"name\": \"lower_bin\", \
     \"update_delay\": 0.05, \
-    \"speed\": 30, \
+    \"speed\": 10, \
     \"lowered_angle\": 40 \
 }";
 export { lowerBinConfig };
@@ -32,7 +32,13 @@ export { lowerBinConfig };
 var digConfig = "{ \
     \"name\": \"dig\", \
     \"update_delay\": 0.1, \
-    \"speed\": 50, \
+    \"speed\": 10, \
     \"duration\": 10 \
 }";
 export { digConfig };
+
+var notFoundConfig = "{ \
+    \"name\": \"NotFound\", \
+    \"states\": -1 \
+}";
+export { notFoundConfig }; 
